b3c566c49e7d090f659286d45474fc34a6fc8b14
William Davidson <wj.davidson@outlook.com>
2023-09-22 15:29:05 +0200

    game session, reset btn render in new round of cards

--- index.php
+++ index.php
@@ -1,25 +1,51 @@
 <?php
 
-$values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-$suits = ['C', 'D', 'H', 'S'];
-$deck = [];
-$stockDeck = [];
-$discardDeck = [];
+session_start();
 
+$deck = [];
+$cardsOnBoard = [];
+
+if (isset($_POST['reset']) && isset($_SESSION['deck'])) {
+    unset($_SESSION['deck']);
+    unset($_SESSION['cardsOnBoard']);
+}
+
+if (!isset($_SESSION['deck'])) {
+    createDeck();
+} else {
+    // get session data
+    $deck = $_SESSION['deck'];
+    $deck = array_splice($deck, 4);
+    $cardsOnBoard = [...$_SESSION['cardsOnBoard'], ...array_splice($deck, 0, 4)];
+
+    // save updated session data
+    $_SESSION['deck'] = $deck;
+    $_SESSION['cardsOnBoard'] = $cardsOnBoard;
+}
 
 function createDeck() {
-    global $suits, $values, $deck;
+    global $deck, $cardsOnBoard;
+
+    $ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    $suits = ['C', 'D', 'H', 'S'];
     
     foreach($suits as $suit) {
-        foreach($values as $value) {
-            $deck[] = [$suit, $value];
+        foreach($ranks as $rank) {
+            $deck[] = [
+                'suit' => $suit,
+                'rank' => $rank
+            ];
         }
     }
     
     shuffle($deck);
-}
 
-createDeck();
+    $cardsOnBoard = array_splice($deck, 0, 4);
+    $_SESSION['cardsOnBoard'] = $cardsOnBoard;
+    
+    $deck = array_splice($deck, 4);
+    $_SESSION['deck'] = $deck;
+}
 
 ?>
 
@@ -34,20 +60,21 @@ createDeck();
 <body>
     <form class="playing-board-form" method="post">
         <section class="playing-board">
-            <?php for ($index = 0; $index < 4; $index++): ?>
-            <div class="card-container card-size">
-                <h3><?= $deck[$index][1] ?></h3>
-                <h1><?= $deck[$index][0] ?></h1>
-                <h3><?= $deck[$index][1] ?></h3>
+            <?php foreach($cardsOnBoard as $card): ?>
+            <div class="card-container card-size <?php echo ($card['suit'] === 'H' || $card['suit'] === 'D') ? 'card-color' : ''; ?>">
+                <h3><?= $card['rank'] ?></h3>
+                <h1><?= $card['suit'] ?></h1>
+                <h3><?= $card['rank'] ?></h3>
             </div>
-            <?php endfor; ?>
+            <?php endforeach; ?>
         </section>
-        <div class="user-interaction-container">
-            <button type="submit" class="back-of-card">
+        <section class="user-interaction-container">
+            <button type="submit" class="back-of-card card-size">
                 <img src="./images/Card_back_01.svg" alt="">
             </button>
-            <div class="discard-pile"></div>    
-        </div>
+            <div class="discard-pile card-size"></div>   
+            <button class="reset-game" name="reset">Reset game</button>
+        </section>
     </form>
 </body>
 </html>
