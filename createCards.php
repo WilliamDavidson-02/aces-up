b32fc34c57329b58165e7e39b4d5597df44df195
William Davidson <wj.davidson@outlook.com>
2023-09-23 20:56:08 +0200

    filter and sort to remove clicked card

--- createCards.php
+++ createCards.php
@@ -3,7 +3,7 @@
 function createDeck() {
     global $deck, $cardsFirstColumn, $cardsSecondColumn, $cardsThirdColumn, $cardsFourthColumn;
 
-    $ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    $ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
     $suits = ['C', 'D', 'H', 'S'];
     
     foreach($suits as $suit) {
--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if (isset($_POST['reset']) && isset($_SESSION['deck'])) {
 
 if (!isset($_SESSION['deck'])) {
     createDeck();
-} else {
+} else if (isset($_POST['newRound'])) {
     // get session data
     $deck = $_SESSION['deck'];
     $cardsForNewRound = array_splice($deck, 0, 4);
@@ -35,6 +35,29 @@ if (!isset($_SESSION['deck'])) {
     $_SESSION['cardsSecondColumn'] = $cardsSecondColumn;
     $_SESSION['cardsThirdColumn'] = $cardsThirdColumn;
     $_SESSION['cardsFourthColumn'] = $cardsFourthColumn;
+} else if (isset($_POST['cardToRemove'])) {
+    $cardsFirstColumn = $_SESSION['cardsFirstColumn'];
+    $cardsSecondColumn = $_SESSION['cardsSecondColumn'];
+    $cardsThirdColumn = $_SESSION['cardsThirdColumn'];
+    $cardsFourthColumn = $_SESSION['cardsFourthColumn'];
+
+    $selectedCard = ['rank' => str_split($_POST['cardToRemove'])[0], 'suit' => str_split($_POST['cardToRemove'])[1]];
+    print_r($selectedCard);
+    $topRowOfCards = [end($cardsFirstColumn), end($cardsSecondColumn), end($cardsThirdColumn), end($cardsFourthColumn)];
+    $topRowOfCards = array_filter($topRowOfCards, function($card) {
+        global $selectedCard;
+
+        return $card['suit'] === $selectedCard['suit'];
+    });
+    uasort($topRowOfCards, function($a, $b) {
+        $letterValue = ['J' => 11, 'Q' => 12, 'K' => '13', 'A' => 14];
+          
+        return ((!is_numeric($a['rank']) ? $letterValue[$a['rank']] : $a['rank']) < (!is_numeric($b['rank']) ? $letterValue[$b['rank']] : $b['rank'])) ? -1 : 1;
+    });
+    print_r($topRowOfCards);
+    // filter await no match suits and sort remaining card by value and if cardToRemove is first then remove it and if there is only one card of the suit don't remove anything
+    // if there are more then 2 cards check if the card to remove is not the last one when sorted
+    // handle move card to another column if the column is empty and the card to remove can't be removed.
 }
 
 $columnContainer = [$cardsFirstColumn, $cardsSecondColumn, $cardsThirdColumn, $cardsFourthColumn];
@@ -54,18 +77,26 @@ $columnContainer = [$cardsFirstColumn, $cardsSecondColumn, $cardsThirdColumn, $c
         <section class="playing-board">
             <?php for ($i = 0; $i < 4; $i++): ?>
                 <div class="cards-column">
-                    <?php foreach($columnContainer[$i] as $key => $card): ?>
-                    <div class="card-container card-size <?php echo ($card['suit'] === 'H' || $card['suit'] === 'D') ? 'card-color' : ''; ?>" style="top: <?= $key * 20; ?>px">
-                        <h3><?= $card['rank']; ?></h3>
-                        <h1><?= $card['suit']; ?></h1>
-                        <h3><?= $card['rank']; ?></h3>
-                    </div>
+                    <?php foreach($columnContainer[$i] as $key => $card): 
+                        if ($key === count($columnContainer[$i]) - 1): ?>
+                        <button type="submit" name="cardToRemove" value="<?= $card['rank'] . $card['suit']; ?>" class="card-container card-size <?php echo ($card['suit'] === 'H' || $card['suit'] === 'D') ? 'card-color' : ''; ?>" style="top: <?= $key * 20; ?>px">
+                            <h3><?= $card['rank']; ?></h3>
+                            <h1><?= $card['suit']; ?></h1>
+                            <h3><?= $card['rank']; ?></h3>
+                        </button>
+                        <?php else: ?>
+                        <div class="card-container card-size <?php echo ($card['suit'] === 'H' || $card['suit'] === 'D') ? 'card-color' : ''; ?>" style="top: <?= $key * 20; ?>px">
+                            <h3><?= $card['rank']; ?></h3>
+                            <h1><?= $card['suit']; ?></h1>
+                            <h3><?= $card['rank']; ?></h3>
+                        </div>
+                        <?php endif; ?>
                     <?php endforeach; ?>
                 </div>
             <?php endfor; ?>
         </section>
         <section class="user-interaction-container">
-            <button type="submit" class="back-of-card card-size">
+            <button type="submit" name="newRound" class="back-of-card card-size">
                 <img src="./images/Card_back_01.svg" alt="">
             </button>
             <div class="discard-pile card-size"></div>   
