1b279ec49a3e4a1b2a7a081a7f570399b0f81c93
William Davidson <wj.davidson@outlook.com>
2023-09-29 15:16:15 +0200

    loops for setting column and deck values and loops for saving to session

--- acesUp.php
+++ acesUp.php
@@ -2,16 +2,36 @@
 session_start();
 
 require_once __DIR__ . '/header.php';
-require __DIR__ . '/createDeck.php';
+require_once __DIR__ . '/createDeck.php';
 
-$newRoundOfCards = array_splice($deck, 0, 4);
+$columnsMap = ['firstColumn', 'secondColumn', 'thirdColumn', 'fourthColumn'];
 
-$cardsOnBoardColumns = [
-    'firstColumn' => [...$_SESSION['firstColumn'], $newRoundOfCards[0]],
-    'secondColumn' => [...$_SESSION['secondColumn'], $newRoundOfCards[1]],
-    'thirdColumn' => [...$_SESSION['thirdColumn'], $newRoundOfCards[2]],
-    'fourthColumn' => [...$_SESSION['fourthColumn'], $newRoundOfCards[3]],
-];
+if (isset($_POST['newGame'])) {
+    unset($_SESSION['deck']);
+    foreach ($columnsMap as $key => $column) {
+        unset($_SESSION[$column]);
+    }
+}
+
+if (!isset($_SESSION['deck'])) {
+    createDeck();
+}
+
+$deck = $_SESSION['deck'];
+
+if (count($deck) > 0) {
+    $newRoundOfCards = array_splice($deck, 0, 4);
+    foreach ($columnsMap as $key => $column) {
+        $cardsOnBoardColumns[$column] = [...$_SESSION[$column], $newRoundOfCards[$key]];
+        $_SESSION[$column] = $cardsOnBoardColumns[$column];
+    }
+    $_SESSION['deck'] = $deck;
+} else {
+    foreach ($columnsMap as $key => $column) {
+        $cardsOnBoardColumns[$column] = $_SESSION[$column];
+    }
+}
+
 ?>
 
 <main class="aces-up-container">
@@ -19,7 +39,7 @@ $cardsOnBoardColumns = [
         <?php foreach ($cardsOnBoardColumns as $column) : ?>
             <div class="card-column">
                 <?php foreach ($column as $key => $card) : ?>
-                    <div class="card card-size" style="top: <?= $key * 20 ?>;">
+                    <div class="card card-size <?= ($card['suit'] == '♥' || $card['suit'] == '♦') ? 'red-card' : ''; ?>" style="top: <?= $key * 40 ?>px;">
                         <h3><?= $card['rank'] ?></h3>
                         <h3><?= $card['suit'] ?></h3>
                         <h3><?= $card['rank'] ?></h3>
@@ -30,10 +50,13 @@ $cardsOnBoardColumns = [
     </section>
     <form class="aces-game-controls" method="post">
         <button class="round-btn card-size">
+            <?php if (count($deck) / 4 <= 5) : ?>
+                <div class="count-down-container"><?= (count($deck) / 4); ?></div>
+            <?php endif; ?>
             <img src="/images/Card_back_01.svg" alt="button of card">
         </button>
         <div class="empty-card-container card-size"></div>
-        <a class="new-game empty-card-container" href="/acesUp.php">New game</a>
+        <button class="new-game empty-card-container" type="submit" name="newGame">New game</button>
     </form>
 </main>
 
--- createDeck.php
+++ createDeck.php
@@ -1,22 +1,25 @@
 <?php
 
-$suits = ['♥', '♦', '♠', '♣'];
-$ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
-$deck = [];
+function createDeck()
+{
+    $suits = ['♥', '♦', '♠', '♣'];
+    $ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+    $deck = [];
 
-foreach ($suits as $suit) {
-    foreach ($ranks as $rank) {
-        $deck[] = [
-            'rank' => $rank,
-            'suit' => $suit
-        ];
+    foreach ($suits as $suit) {
+        foreach ($ranks as $rank) {
+            $deck[] = [
+                'rank' => $rank,
+                'suit' => $suit
+            ];
+        }
     }
+
+    shuffle($deck);
+
+    $_SESSION['deck'] = $deck;
+    $_SESSION['firstColumn'] = [];
+    $_SESSION['secondColumn'] = [];
+    $_SESSION['thirdColumn'] = [];
+    $_SESSION['fourthColumn'] = [];
 }
-
-shuffle($deck);
-
-
-$_SESSION['firstColumn'] = [];
-$_SESSION['secondColumn'] = [];
-$_SESSION['thirdColumn'] = [];
-$_SESSION['fourthColumn'] = [];
